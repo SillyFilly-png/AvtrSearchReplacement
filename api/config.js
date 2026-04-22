620286fbf4ba081316e1cd18765370cdef6b2436
SillyF <byes75377@gmail.com>
2026-04-22 04:39:55 -0400

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,3 @@
 module.exports = [
-    "https://api.avtrdb.com/v2/avatar/search?query=",
-    "https://api.avtr.zip/v1/search?q=",
-    "https://requi.dev/vrcx_search.php?search="
+    "https://api.avtrdb.com/v2/avatar/search?query="
 ];
